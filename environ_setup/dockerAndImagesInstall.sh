49c251147258d4cb8e557562f135767eb0651cf6
chi-hung <chi-hung.weng@gmx.de>
2017-11-13 16:57:01 +0800

    updated

--- environ_setup/dockerAndImagesInstall.sh
+++ environ_setup/dockerAndImagesInstall.sh
@@ -19,3 +19,19 @@ dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
 
 # Test if nvidia-smi works from a docker container.
 nvidia-docker run --rm nvidia/cuda nvidia-smi
+
+# obtain some popular images
+docker pull nvidia/cuda
+docker pull nvidia/caffe
+docker pull nvidia/digits
+docker pull pytorch/pytorch
+docker pull caffe2ai/caffe2
+#docker pull microsoft/cntk
+#docker pull tensorflow/tensorflow:latest-gpu-py3
+
+# Obtain Keras images built and maintained by me.
+# Further info: https://github.com/chi-hung/DockerbuildsKeras
+docker pull honghu/keras:tf-cu9-dnn7-py3-avx2
+docker pull honghu/keras:cntk-cu8-dnn6-py3
+docker pull honghu/keras:mx-cu9-dnn7-py3
+docker pull honghu/keras:theano-cu9-dnn7-py3
